06e81f0df28ce88c180331de2e63540cb9f04e2a
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2026-06-21 10:35:36 +0200

    Create initramfs.sh

---
+++ common/initramfs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+shopt -s nullglob
+
+KVER=$(ls /usr/lib/modules | head -n1)
+
+depmod -a "$KVER"
+export DRACUT_NO_XATTR=1
+/usr/bin/dracut \
+  --no-hostonly \
+  --kver "$KVER" \
+  --reproducible \
+  --zstd -v \
+  --add ostree --add fido2 \
+  -f "/usr/lib/modules/$KVER/initramfs.img"
+
+chmod 0600 "/usr/lib/modules/$KVER/initramfs.img"

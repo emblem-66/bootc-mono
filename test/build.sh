1164a9de7f76a6d8b03dcddfaad25b9bba9bf7e2
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2026-06-21 10:38:42 +0200

    Update build.sh

--- test/build.sh
+++ test/build.sh
@@ -123,16 +123,16 @@ rm -rf /boot/*
 
 
 
-KVER=$(ls /usr/lib/modules | head -n1)
+#KVER=$(ls /usr/lib/modules | head -n1)
 
-depmod -a "$KVER"
-export DRACUT_NO_XATTR=1
-/usr/bin/dracut \
-  --no-hostonly \
-  --kver "$KVER" \
-  --reproducible \
-  --zstd -v \
-  --add ostree --add fido2 \
-  -f "/usr/lib/modules/$KVER/initramfs.img"
+#depmod -a "$KVER"
+#export DRACUT_NO_XATTR=1
+#/usr/bin/dracut \
+#  --no-hostonly \
+#  --kver "$KVER" \
+#  --reproducible \
+#  --zstd -v \
+#  --add ostree --add fido2 \
+#  -f "/usr/lib/modules/$KVER/initramfs.img"
 
-chmod 0600 "/usr/lib/modules/$KVER/initramfs.img"
+#chmod 0600 "/usr/lib/modules/$KVER/initramfs.img"
